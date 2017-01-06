b7e7270d9b1018f1742bbcc744010bdf12cc6447
dodo <lhuibo@gmail.com>
2017-01-06 19:16:26 +0800

    參考資料

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-        <title>hello vue2 && vue-router 2</title>
+        <title>hello vue2 & vue-router 2</title>
     </head>
 
     <body>
@@ -23,23 +23,25 @@
             <router-view class="view"></router-view>
         </div>
 
-        <h3>step</h3>
+        <h3>詳步驟</h3>
         <ul>
-            <li>配置路由web.php及模板vue.blade.php</li>
             <li>配置package.json</li>
             <li>安装依赖 npm install</li>
 
+            <li>配置路由web.php及模板vue.blade.php</li>
             <li>引入vue及vue-router開發，見js/app.js</li>
 
             <li>编译合并 JS/CSS 代码，gulp，如需实时编译，可输入 gulp watch</li>
             <li>http://l.com/vue</li>
         </ul>
 
-        <h3>參考資料</h3>
+        <h3>技術棧</h3>
         <ul>
-            <li><a href="https://cn.vuejs.org/v2/guide/" target="_blank">vue2</a></li>
-            <li><a href="https://router.vuejs.org/zh-cn/" target="_blank">vue-router 2</a></li>
-            <li><a href="http://laravelacademy.org/post/5962.html" target="_blank">laravel elixir</a></li>
+            <li><a href="https://cn.vuejs.org/v2/guide/" target="_blank">vue2</a><br/> Vue.js是一套构建用户界面的 渐进式框架。</li>            
+            <li><a href="https://vuex.vuejs.org/zh-cn/intro.html" target="_blank">vuex</a><br/> Vuex 是一个专为 Vue.js 应用程序开发的状态管理模式。</li>
+            <li><a href="https://router.vuejs.org/zh-cn/" target="_blank">vue-router 2</a><br/> vue-router是Vue.js官方的路由插件，它和vue.js是深度集成的，适合用于构建单页面应用。</li>
+            <li><a href="http://laravelacademy.org/post/5962.html" target="_blank">laravel elixir</a><br/>Laravel Elixir 提供 API 用于定义基本的Gulp任务。其支持一些通用的 CSS 和 JavaScript 预处理器，甚至测试工具。</li>
+
         </ul>
 
         <script type="text/javascript">
